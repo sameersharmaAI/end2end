bf00df7f08461919a456f4cf321a419b1460e1aa
Sameer Sharma <85783007+sameersharmaAI@users.noreply.github.com>
2024-09-10 20:10:47 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
-
+import os
 from sklearn.preprocessing import StandardScaler
 
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
@@ -43,4 +43,5 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 if __name__=="__main__":
-    app.run(host="127.0.0.1",port=8000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
